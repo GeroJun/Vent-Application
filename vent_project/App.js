23f308859f4a775d83e29d6e39e4b29bfe678af6
Isaiah Chang <ichang20@apu.edu>
2024-06-01 14:26:25 +0800

    home page with test buttons

--- vent_project/App.js
+++ vent_project/App.js
@@ -1,10 +1,16 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import TestScreen1 from "./src/screens/TestScreen1";
+import TestScreen2 from "./src/screens/TestScreen2";
+import TestScreen3 from "./src/screens/TestScreen3";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Test1: TestScreen1,
+    Test2: TestScreen2,
+    Test3: TestScreen3,
   },
   {
     initialRouteName: "Home",
