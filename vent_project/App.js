049152c62f8620a8f43c4045c156e19cd3219ff9
Paul <532paul@gmail.com>
2024-06-20 22:23:54 -0700

    Updated React Navigation & Stack versions
    + adjusted app.js to use the NavigationContainer.
    Set the default screen to homescreen for now.

--- vent_project/App.js
+++ vent_project/App.js
@@ -1,26 +1,48 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import HomeScreen from "./src/screens/HomeScreen";
-import TestScreen1 from "./src/screens/TestScreen1";
-import TestScreen2 from "./src/screens/TestScreen2";
-import TestScreen3 from "./src/screens/TestScreen3";
-import Drawer from "./src/components/Drawer";
-import LoginScreen from "./src/screens/LoginScreen";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import TestScreen1 from './src/screens/TestScreen1';
+import TestScreen2 from './src/screens/TestScreen2';
+import TestScreen3 from './src/screens/TestScreen3';
+import Drawer from './src/components/Drawer';
+import LoginScreen from "./src/screens/LoginScreen"
 
-const navigator = createStackNavigator(
-  {
-    Login: LoginScreen,
-    Home: HomeScreen,
-    Test1: TestScreen1,
-    Test2: TestScreen2,
-    Test3: TestScreen3,
-  },
-  {
-    initialRouteName: "Login",
-    defaultNavigationOptions: {
-      title: "Vent Application",
-    },
-  }
-);
 
-export default createAppContainer(navigator);
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'App' }}
+        />
+        <Stack.Screen
+          name="Test1"
+          component={TestScreen1}
+          options={{ title: 'Test Screen 1' }}
+        />
+        <Stack.Screen
+          name="Test2"
+          component={TestScreen2}
+          options={{ title: 'Test Screen 2' }}
+        />
+        <Stack.Screen
+          name="Test3"
+          component={TestScreen3}
+          options={{ title: 'Test Screen 3' }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options = {{title: 'Login Screen'}}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
